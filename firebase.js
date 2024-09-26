66a9144f494446e3d45544ad625414cbc2dc210f
Michael <ramosjo@students.nu-baliwag.edu.ph.com>
2024-09-26 16:18:10 +0800

    login and homepage

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js"; // Import authentication service
 
+// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCOC-a_k4IEpdeW_Rms6v_V8jBgdTO2k5M",
   authDomain: "tradeare-us-2nx4zp.firebaseapp.com",
@@ -10,7 +12,13 @@ const firebaseConfig = {
   appId: "1:752862844692:web:5d656e58696c44d5ef9401"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
 const db = getFirestore(app);
 
-export { db };
+// Initialize Firebase Authentication
+const auth = getAuth(app);
+
+export { db, auth };
